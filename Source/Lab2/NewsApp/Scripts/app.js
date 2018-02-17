33942cf8dfdc239e6d60ee22fb46b49df57d3f1b
sgvhb <sgvhb@mail.umkc.edu>
2018-02-16 22:58:26 -0600

    lab2
    lab2

---
+++ Source/Lab2/NewsApp/Scripts/app.js
@@ -0,0 +1,118 @@
+﻿var app = angular
+    .module('NewsApp', [])
+    .controller('NewsAppController', function ($scope, $http, $log) {
+
+        $scope.getPosts = function () {
+
+            $("#news").dataTable({
+                responsive: true,
+                "columnDefs": [
+                    { "orderable": false, "width": "200px", "targets": [2] }
+                ],
+                "columns": [
+                    { "data": "Description", className: "all" },
+                    { "data": "Image", className: "all" },
+                    { "data": "TimeStamp ", className: "all" },
+                    
+                ]
+
+            });
+
+            if ($scope.topic != null && $scope.src != null) {
+                var url = 'https://newsapi.org/v2/everything?' +
+                    'q=' + $scope.topic + '&' +
+                    'sources=' + $scope.src + '&' +
+                    'sortBy=popularity&' +
+                    'apiKey=bdc95f0d4eb64c4f8dbd7a8b86515a36';
+            }
+            else if ($scope.topic == null && $scope.src != null) {
+                var url = 'https://newsapi.org/v2/everything?' +
+                    'sources=' + $scope.src + '&' +
+                    'sortBy=popularity&' +
+                    'apiKey=bdc95f0d4eb64c4f8dbd7a8b86515a36';
+            }
+            else if ($scope.topic != null && $scope.src == null) {
+                var url = 'https://newsapi.org/v2/everything?' +
+                    'q=' + $scope.topic + '&' +
+                    'sortBy=popularity&' +
+                    'apiKey=bdc95f0d4eb64c4f8dbd7a8b86515a36';
+            }
+
+            $http({
+                method: 'GET',
+                url: url
+            }).then(function (success) {
+                $scope.posts = success.data.articles;
+            }, function (error) {
+                $scope.error = error.data;
+                $log.info(error);
+            });
+
+        }
+    });//controller end
+
+
+/*
+var url = 'https://newsapi.org/v2/top-headlines?' +
+    'country=us&' +
+    'apiKey=34277a7a131b4b81a6c3b36aeaa106b8';
+var req = new Request(url);
+
+HttpService.getPost()
+    .then(function(response) {
+        $scope.post = response;
+    });
+
+.controller('AppCtrl', function($scope,HttpService) {
+    HttpService.getPost()
+        .then(function(response) {
+            $scope.post = response;
+        });
+    HttpService.display()
+    { getPost();
+    }
+}),
+
+.service('HttpService', function($http) {
+        return {
+            getPost: function () {
+                // $http returns a promise, which has a then function, which also returns a promise.
+                return $http.get('url')
+                        .then(function (response) {
+                            // In the response, resp.data contains the result. Check the console to see all of the data returned.
+                            console.log('Get Post', response);
+                            return response.data;
+                        });
+            }
+        };
+});
+
+.init()
+{
+    getPost();
+}
+
+$scope.display = function () {
+    search();
+};
+
+
+
+
+
+
+    /!*search.controller ('NewsAppController',[$scope ,function($scope){
+        $scope.items = ['headlines','images','timestamp',{text:"class",image:getSelection(Window.OPEN),done:false}];
+
+        $scope.display = function () {
+            /!*if($scope.items.display != EMPTY)
+            {
+                $scope.items.push({text:$scope.title,image:$scope.getImageData,done:false});
+            }*!/
+            $scope.items.push({text:$scope.title,image:$scope.getImageData(url),done:false});
+        };
+        $scope.display= function (items) {
+            //display
+                getPost();
+        };
+    }]);*!/*/
---
+++ Source/Lab2/TictactoeGame/scripts/news.js
@@ -0,0 +1,94 @@
+﻿    var outputJSON;
+    var topicFlag = 0;
+    var sourceFlag = 0;
+    var i = 0;
+    var current_username;
+
+    //get user id passed from index page from the url
+    var url = window.location.href.split('?');
+    var current_user_id = url[url.length - 1];
+
+    var users = JSON.parse(localStorage.getItem("news_app_users"));
+    for (i = 0 ; i < users.length; i++){
+        if (users[i].user_id == current_user_id) {
+            current_username = users[i].userName;
+            $('#current_user').text("Hi " + users[i].userName + "...!!!");
+        }
+    }
+
+    $('#submit').click(function () {
+        $('#contentPanel').empty();
+        //get topic
+        var topic = $('#topic option:selected').val();
+        if (topic.length > 0 && topic != null && topic != '' && topic != 'Select Topic') {
+            topicFlag = 1;
+        }
+
+        //get source
+        var source = $('#source option:selected').val();
+        if (source.length > 0 && source != null && source != '' && source != 'Select Source') {
+            sourceFlag = 1;
+        }
+
+        //give appropriate API depending on input
+        if(topicFlag == 1 && sourceFlag == 1){
+            var url = 'https://newsapi.org/v2/everything?q=' + topic+ '&sources=' + source +
+                        '&apiKey=11435bb600564433b2771bafd5527899';
+        }
+
+        else if (topicFlag == 0 && sourceFlag == 1) {
+            var url = 'https://newsapi.org/v2/everything?sources=' + source +
+                        '&apiKey=11435bb600564433b2771bafd5527899';
+        }
+
+        else if (topicFlag == 1 && sourceFlag == 0) {
+            var url = 'https://newsapi.org/v2/everything?q=' + topic + 
+                        '&apiKey=11435bb600564433b2771bafd5527899';
+        }
+
+        else {
+            var url = 'https://newsapi.org/v2/top-headlines?country=us' +
+                       '&apiKey=11435bb600564433b2771bafd5527899';
+        }
+
+        //Read JSON from API
+        var xhttp = new XMLHttpRequest();
+        xhttp.open('GET', url, false);
+        xhttp.send();
+        response = xhttp;
+        outputJSON = JSON.parse(response.responseText);
+        console.log(outputJSON);
+        for (var i = 1; i <= outputJSON.totalResults; i++) {
+            if (outputJSON.articles[i - 1].urlToImage !=null) {
+                var myCol = $('<div class="col-md-4"></div>');
+                var myPanel = $('<div class="card card-outline-info" id="Card' + i + '">' +
+                    '<a href="' + outputJSON.articles[i - 1].url + '" target="_blank">' +
+                    '<img class="card-img-top" style="height:200px; width: 100%" src="' + outputJSON.articles[i - 1].urlToImage +
+                    '"></a>' +
+                    '<div class="card-block">' +
+                    '<h4 class="card-title">' + outputJSON.articles[i - 1].title + '</h4>' +
+                    '<p class="card-text">' + outputJSON.articles[i - 1].description +
+                    '<span class="pull-right"><br/>' + outputJSON.articles[i - 1].publishedAt + '</span>' +
+                    '</p>' +
+                    '</div></div>');
+                myPanel.appendTo(myCol);
+                myCol.appendTo('#contentPanel');
+            }
+        }
+    });
+    
+     
+    $('#logout_btn').on('click', function(e){
+        window.location.href = "../index.html";
+    });
+
+
+
+
+
+
+
+
+
+
+
